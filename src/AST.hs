6c294e9e36b577c821aee3abc1e8ffc003c6ef89
guillaume abel <guillaume.abel@epitech.eu>
2023-11-28 15:51:01 +0100

    Add AST

--- src/AST.hs
+++ src/AST.hs
@@ -7,4 +7,15 @@
 
 module AST
     (
+        Symbol,
+        Atom (Number, Symbol),
+        Tree (Node, Leaf),
     ) where
+
+import Data.Int (Int64)
+
+type Symbol = String
+
+data Atom = Number Int64 | Symbol Symbol
+
+data Tree = Node Symbol [Tree] | Leaf Atom
