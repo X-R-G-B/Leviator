b644978336f3b2455a6067e7892a08de24dc0c22
guillaume abel <guillaume.abel@epitech.eu>
2023-12-02 19:31:01 +0100

    Interpreter V.0: Add textToAST parser, handling int64, bool and functions

--- src/AST.hs
+++ src/AST.hs
@@ -8,14 +8,28 @@
 module AST
     (
         Symbol,
-        Atom (Number, Symbol),
+        Atom (Number, Symbol, Boolean),
         Tree (Node, Leaf),
+        showMaybeTree
     ) where
 
 import Data.Int (Int64)
 
 type Symbol = String
 
-data Atom = Number Int64 | Symbol Symbol
+data Atom = Number Int64 | Symbol Symbol | Boolean Bool
 
-data Tree = Node Symbol [Tree] | Leaf Atom
+data Tree = Node Symbol (Maybe Tree) (Maybe Tree) | Leaf Atom
+
+showMaybeTree :: Maybe Tree -> String
+showMaybeTree Nothing = "Nothing"
+showMaybeTree (Just tree) = show tree
+
+instance Show Atom where
+  show (Number value) = "Number : " ++ show value
+  show (Symbol value) = "Symbol : " ++ value
+  show (Boolean value) = "Boolean : " ++ show value
+
+instance Show Tree where
+  show (Node value left right) = "Node : \"" ++ value ++ "\" left : \"" ++ showMaybeTree left ++ "\" right : \"" ++ showMaybeTree right ++ "\""
+  show (Leaf value) = "Leaf : " ++ show value
--- src/TextToAST.hs
+++ src/TextToAST.hs
@@ -7,4 +7,57 @@
 
 module TextToAST
     (
+        textToAST
     ) where
+
+import AST
+import Data.Int (Int64)
+import Data.Char (isDigit)
+import Debug.Trace
+
+isFunction :: String -> Bool
+isFunction [] = False
+isFunction (x:_) | x == '(' = True
+                 | otherwise = False
+
+skipableChar :: Char -> Bool
+skipableChar x = x == ' ' || x == '\t'
+
+notSkipableChar :: Char -> Bool
+notSkipableChar x = not (skipableChar x)
+
+stringIsNumber :: String -> Bool
+stringIsNumber [] = False
+stringIsNumber (x:[]) | Data.Char.isDigit x = True
+stringIsNumber (x:xs) | Data.Char.isDigit x = stringIsNumber xs
+                      | otherwise = False
+
+nextToParse :: String -> String
+nextToParse [] = []
+nextToParse (x:xs) | skipableChar x = nextToParse xs
+nextToParse str = dropWhile notSkipableChar str
+
+createNode :: Symbol -> String -> Maybe Tree
+createNode [] _ = Nothing
+createNode _ [] = Nothing
+createNode (_:xs) str = Just (Node xs (textToAST str) (textToAST (nextToParse str)))
+
+stringIsBool :: String -> Bool
+stringIsBool str = str == "#t" || str == "#f"
+
+createBool :: String -> Maybe Tree
+createBool str | str == "#t" = Just (Leaf (Boolean True))
+               | str == "#f" = Just (Leaf (Boolean False))
+               | otherwise = Nothing
+
+treeFromAtom :: String -> String -> Maybe Tree
+treeFromAtom [] _ = Nothing
+treeFromAtom split _ | stringIsNumber split = Just (Leaf (AST.Number (read split :: Data.Int.Int64)))
+                     | stringIsBool split = createBool split
+treeFromAtom split str | isFunction split = createNode split (init str)
+                       | otherwise = Just (Leaf (Symbol split))
+
+textToAST :: String -> Maybe Tree
+textToAST [] = Nothing
+textToAST (x:xs) | skipableChar x = textToAST xs
+textToAST str = treeFromAtom (takeWhile notSkipableChar str) (dropWhile notSkipableChar str)
