f9c87b54f690079589859d3cc7ab1834f2a875a8
Xavier Mitault <xavier.mitault@epitech.eu>
2023-12-02 21:06:18 +0000

    Fix test maybe
    PATCH

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,9 +13,13 @@ tests = testGroup "Tests" [unitTests]
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "Basic AST creation 0" $
-      assertEqual "define x 42" (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)]) (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)])
+      assertEqual "define x 42" (Node "define" (Just $ Leaf (Symbol "x")) (Just $ Leaf (Number 42))) (Node "define" (Just $ Leaf (Symbol "x")) (Just $ Leaf (Number 42)))
   , testCase "Basic AST creation 1" $
       assertEqual "foo" (Leaf (Symbol "foo")) (Leaf (Symbol "foo"))
   , testCase "Basic AST creation 2" $
       assertEqual "42" (Leaf (Number 42)) (Leaf (Number 42))
+  , testCase "Basic AST creation 3" $
+      assertEqual "#f" (Leaf (Boolean False)) (Leaf (Boolean False))
+  , testCase "Basic AST creation 4" $
+      assertEqual "#t" (Leaf (Boolean True)) (Leaf (Boolean True))
   ]
