7879e69b416ccef62630d79780cf430d798382a8
Xavier Mitault <xavier.mitault@epitech.eu>
2023-12-02 16:16:19 +0000

    Add basic test for ast
    MINOR

--- src/AST.hs
+++ src/AST.hs
@@ -19,3 +19,21 @@ type Symbol = String
 data Atom = Number Int64 | Symbol Symbol
 
 data Tree = Node Symbol [Tree] | Leaf Atom
+
+instance Eq Atom where
+    Number a == Number b = a == b
+    Symbol a == Symbol b = a == b
+    _ == _ = False
+
+instance Show Atom where
+    show (Number a) = "Number:'" ++ show a ++ "'"
+    show (Symbol a) = "Symbol:'" ++ a ++ "'"
+
+instance Eq Tree where
+    Node a as == Node b bs = a == b && as == bs
+    Leaf a == Leaf b = a == b
+    _ == _ = False
+
+instance Show Tree where
+    show (Node a as) = "Node:'" ++ a ++ "'{" ++ show as ++ "}"
+    show (Leaf a) = "Leaf:'" ++ show a ++ "'"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import AST
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [ testCase "Basic AST creation 0" $
+      assertEqual "define x 42" (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)]) (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)])
+  , testCase "Basic AST creation 1" $
+      assertEqual "foo" (Leaf (Symbol "foo")) (Leaf (Symbol "foo"))
+  , testCase "Basic AST creation 2" $
+      assertEqual "42" (Leaf (Number 42)) (Leaf (Number 42))
+  ]
