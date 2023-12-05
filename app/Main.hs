fb75ee5e1a5d351d85f8571f1a56c33d05b654fd
tenshi <tenshii.tv@gmail.com>
2023-12-05 02:04:54 +0100

    add working defines with basic operations (+-*)

--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,32 @@ import ComputeAST
 import AST
 import Defines
 
+-- Foo = 42
 testTreeDefine :: Tree
--- data Tree = Node Symbol (Maybe Tree) (Maybe Tree) | Leaf Atom
-testTreeDefine = Node "define" (Just (Leaf (Symbol "foo"))) (Just (Leaf (Number 21)))
+testTreeDefine = Node "define" (Just (Leaf (Symbol "foo"))) (Just (Leaf (Number 42)))
 
+-- Bar = 21
 testTreeDefine2 :: Tree
-testTreeDefine2 = Node "define" (Just (Leaf (Symbol "bar"))) (Just (Leaf (Number 42)))
+testTreeDefine2 = Node "define" (Just (Leaf (Symbol "bar"))) (Just (Leaf (Number 21)))
+
+-- 21 + 21 (result = 42)
+testTreeAddition :: Tree
+testTreeAddition = Node "+" (Just (Leaf (Number 21))) (Just (Leaf (Number 21)))
+
+-- Foo + 42 (result = 42 + 42 = 84)
+testTreeAdditiondefine :: Tree
+testTreeAdditiondefine = Node "+" (Just (Leaf (Symbol "foo"))) (Just (Leaf (Number 42)))
+
+-- Foo + Bar (result = 42 + 21 = 63)
+testTreeAdditiondefine2 :: Tree
+testTreeAdditiondefine2 = Node "+" (Just (Leaf (Symbol "foo"))) (Just (Leaf (Symbol "bar")))
 
 main :: IO ()
-main = computeASTs [testTreeDefine, testTreeDefine2]
+main = computeASTs (Env [])
+    [   
+        testTreeDefine,
+        testTreeDefine2,
+        testTreeAddition,
+        testTreeAdditiondefine,
+        testTreeAdditiondefine2
+    ]
--- src/ComputeAST.hs
+++ src/ComputeAST.hs
@@ -12,11 +12,65 @@ module ComputeAST
 
 import AST
 import Defines
+import Data.Int (Int64)
 
--- Temporary function to compute defines only
-computeAST :: Env -> Tree -> Env
-computeAST env tree = registerDefine env tree
+-- TODO: Handle case where the define is a string / not defined
+getSymbolValue :: Env -> String -> Int64
+getSymbolValue (Env []) _ = 0
+getSymbolValue (Env ((Define symbol value):rest)) symbolToFind
+    | symbol == symbolToFind = case value of
+        (Number number) -> number
+        (Symbol _) -> 0
+    | otherwise = getSymbolValue (Env rest) symbolToFind
+
+------------ ADD SUB MUL FUNCTIONS ------------
+
+-- Compute a "+" node, using defines if needed
+-- Todo: See for better error handling in last line
+additionTree :: Env -> Tree -> Int64
+additionTree _ (Node "+" (Just (Leaf (Number left))) (Just (Leaf (Number right)))) = left + right
+additionTree env (Node "+" (Just (Leaf (Number left))) (Just (Leaf (Symbol right)))) = left + getSymbolValue env right
+additionTree env (Node "+" (Just (Leaf (Symbol left))) (Just (Leaf (Number right)))) = getSymbolValue env left + right
+additionTree env (Node "+" (Just (Leaf (Symbol left))) (Just (Leaf (Symbol right)))) = getSymbolValue env left + getSymbolValue env right
+additionTree _ _ = 0
+
+-- Compute a "-" node, using defines if needed
+-- Todo: See for better error handling in last line
+substactionTree :: Env -> Tree -> Int64
+substactionTree _ (Node "-" (Just (Leaf (Number left))) (Just (Leaf (Number right)))) = left - right
+substactionTree env (Node "-" (Just (Leaf (Number left))) (Just (Leaf (Symbol right)))) = left - getSymbolValue env right
+substactionTree env (Node "-" (Just (Leaf (Symbol left))) (Just (Leaf (Number right)))) = getSymbolValue env left - right
+substactionTree env (Node "-" (Just (Leaf (Symbol left))) (Just (Leaf (Symbol right)))) = getSymbolValue env left - getSymbolValue env right
+substactionTree _ _ = 0
+
+-- Compute a "*" node, using defines if needed
+-- Todo: See for better error handling in last line
+multiplicationTree :: Env -> Tree -> Int64
+multiplicationTree _ (Node "*" (Just (Leaf (Number left))) (Just (Leaf (Number right)))) = left * right
+multiplicationTree env (Node "*" (Just (Leaf (Number left))) (Just (Leaf (Symbol right)))) = left * getSymbolValue env right
+multiplicationTree env (Node "*" (Just (Leaf (Symbol left))) (Just (Leaf (Number right)))) = getSymbolValue env left * right
+multiplicationTree env (Node "*" (Just (Leaf (Symbol left))) (Just (Leaf (Symbol right)))) = getSymbolValue env left * getSymbolValue env right
+multiplicationTree _ _ = 0
+
+------------ ComputeNode ------------
+
+computeNode :: Env -> Tree -> Int64
+computeNode env tree@(Node "+" _ _) = additionTree env tree
+computeNode env tree@(Node "-" _ _) = substactionTree env tree
+computeNode env tree@(Node "*" _ _) = multiplicationTree env tree
+-- TODO: Error handling
+computeNode _ _ = 0
+
+------------ COMPUTE TREE ------------
+
+-- Call appropriate function depending on the node
+computeAST :: Env -> Tree -> IO Env
+computeAST env tree@(Node "define" _ _) = return $ registerDefine env tree
+computeAST env tree = print (computeNode env tree) >> return env
 
 -- Call computeAST on every tree in the list
-computeASTs :: [Tree] -> IO ()
-computeASTs tree = print (foldl computeAST (Env []) tree) >> return ()
+computeASTs :: Env -> [Tree] -> IO ()
+computeASTs _ [] = return ()
+computeASTs env (tree:rest) = do
+    updatedEnv <- computeAST env tree
+    computeASTs updatedEnv rest
