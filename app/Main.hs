960e364afc203a765a0e238625931dcffa9bd318
tenshi <tenshii.tv@gmail.com>
2023-12-06 03:15:34 +0100

    Clean files

--- app/Main.hs
+++ app/Main.hs
@@ -5,25 +5,5 @@
 -- Main
 -}
 
-import ComputeAST
-import AST
-import Defines
-
--- Foo = 42
-test1 :: Tree
-test1 = Node "define" (Just (Leaf (Symbol "foo"))) (Just (Leaf (Number 42)))
-
--- 21 + 21 (result = 42)
-test2 :: Tree
-test2 = Node "+" (Just (Leaf (Number 21))) (Just (Leaf (Number 21)))
-
--- bool = true
-test3 :: Tree
-test3 = Leaf (Boolean True)
-
--- bool = false
-test4 :: Tree
-test4 = Leaf (Boolean False)
-
 main :: IO ()
-main = print $ computeAllAST (Env []) [test1, test2, test3, test4]
+main = print "Koaky koaky"
