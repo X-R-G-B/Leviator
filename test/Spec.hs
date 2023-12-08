45b9331b49181144c88d4325277bd443314cd345
Tenshi <tenshii.tv@gmail.com>
2023-12-08 13:47:16 +0100

    Add compute tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,7 +8,7 @@ main :: IO ()
 main = defaultMainWithIngredients (htmlRunner : defaultIngredients) tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [unitTests, computeTests]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
@@ -18,4 +18,14 @@ unitTests = testGroup "Unit tests"
       assertEqual "foo" (Leaf (Symbol "foo")) (Leaf (Symbol "foo"))
   , testCase "Basic AST creation 2" $
       assertEqual "42" (Leaf (Number 42)) (Leaf (Number 42))
+  ]
+
+computeTests :: TestTree
+computeTests = testGroup "Compute tests"
+  [ testCase "Basic compute 0" $
+      assertEqual "define x 42" (Atom 42) (computeAst (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)]))
+  , testCase "Basic compute 1" $
+      assertEqual "define x 42 and do x + x" (Leaf (Number 42)) (compute (Node "define" [Leaf (Symbol "x"), Leaf (Number 42)]))
+  , testCase "Basic compute 2" $
+      assertEqual "42 + 42" (Atom 84) (computeAst (Node "+" [Leaf (Number 42), Leaf (Number 42)]))
   ]
