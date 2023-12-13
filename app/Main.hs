2619affd4c496dc5d435dff272f64e1c23caf7df
guillaume abel <guillaume.abel@epitech.eu>
2023-12-13 14:43:37 +0100

    Add current progress

--- app/Main.hs
+++ app/Main.hs
@@ -9,20 +9,13 @@ import Computing.ComputeAST
 import Parsing.Parser
 import Types
 
-prompt :: String
-prompt = "> "
-
 printErrors :: (Env) -> IO ()
 printErrors (Env defines_ []) = printErrors (Env defines_ ["Unable to compute"])
-printErrors (Env defines_ errors_) = do
-  mapM_ putStrLn errors_
-  handleInput (Env defines_ [])
+printErrors (Env defines_ errors_) = mapM_ putStrLn errors_ >> handleInput (Env defines_ [])
 
 checkComputing :: (Env, Maybe Result) -> IO ()
 checkComputing (env, Nothing) = printErrors env
-checkComputing (env, Just result) = do
-  putStrLn (show result)
-  handleInput env
+checkComputing (env, Just result) = putStrLn (show result) >> handleInput env
 
 checkParsing :: Maybe (Tree, String) -> Env -> IO ()
 checkParsing Nothing _ = return ()
@@ -33,10 +26,7 @@ checkInput ":q" _ = return ()
 checkInput input env = checkParsing (runParser (parseTree) input) env
 
 handleInput :: Env -> IO ()
-handleInput env = do
-  putStr prompt
-  line <- getLine
-  checkInput line env
+handleInput env = getLine >>= (\x -> checkInput x env)
 
 main :: IO ()
 main = handleInput (Env [] [])
