9d3970ff5e26c9ad038c3d15a9f7f29fc56449f7
Xavier Mitault <xavier.mitault@epitech.eu>
2023-12-15 20:23:21 +0100

    Fix norm

--- app/Args.hs
+++ app/Args.hs
@@ -51,14 +51,11 @@ parseArgs args =
 printHelp :: IO ()
 printHelp = putStr help
     where
-        line1 = "Usage: koaky [OPTION]\n\n"
-        line2 = "Interpret Lisp\n"
+        line1 = "Usage: koaky [OPTION]\n\nInterpret Lisp\n"
         line2a = "With no options, koaky reads from standard input.\n\n"
         line3 = "Options:\n"
         line4 = "\t-h, --help\n\t\tDisplay this help and exit\n"
         line5 = "\t-v, --version\n\t\tOutput version information and exit\n"
         line6 = "\t-f FILE, --file FILE\n\t\tRead FILE and Interpret it\n"
         line7 = "\t-\n\t\tRead from standard input and Interpret it\n"
-        help1 = line1 ++ line2 ++ line2a
-        help2 = line3 ++ line4 ++ line5 ++ line6 ++ line7
-        help = help1 ++ help2 
+        help = line1 ++ line2a ++ line3 ++ line4 ++ line5 ++ line6 ++ line7
