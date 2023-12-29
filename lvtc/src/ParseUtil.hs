d844b8ca5db4b3c8b721bcf0a35d273ae9ae1954
Xavier Mitault <xavier.mitault@epitech.eu>
2023-12-29 19:01:08 +0100

    Adjust parseAllCharUntil to not include the until + Add alias
    MAJOR

--- lvtc/src/ParseUtil.hs
+++ lvtc/src/ParseUtil.hs
@@ -70,4 +70,4 @@ parseAllCharUntil str = Parser f
         f [] = empty
         f (x:xs) = case runParser (parseString str) (x:xs) of
                 Nothing -> runParser ((x :) <$> parseAllCharUntil str) xs
-                Just (y, ys) -> Just (y, ys)
+                Just (_, ys) -> Just ([], ys)
